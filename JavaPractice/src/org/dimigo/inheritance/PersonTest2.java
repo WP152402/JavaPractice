963bf5007b94d1330608025eb10507b919f7f698
고오오오오오영으으으으으은 <@DESKTOP-3M4G4R8>
2017-05-11 15:28:06 +0900

    d

---
+++ JavaPractice/src/org/dimigo/inheritance/PersonTest2.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ * 	|_PersonTest
+ * 
+ * 1.개요 :
+ * 2. 작성일 : 2017. 5. 9.
+ * </pre>
+ * 
+ * @author 		: 고오오오오오영으으으으으은
+ * @version		: 1.0
+ */
+public class PersonTest2 {
+	public static void main(String[] args) {
+		Person[] person = {
+				new Person("Tom"),
+				new Korean("홍길동"),
+				new Japanese("다나카"),
+				new Chinese("왕밍")
+		};
+		for(Person p : person){
+			System.out.println(p);
+			p.sayHello();
+			p.sayBye();
+			System.out.println();
+		}
+	}
+
+}
